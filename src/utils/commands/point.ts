bc211c124ab60b882ee2f722452c597e4d73236c
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-25 20:32:05 +0900

    [add]utility for command only

---
+++ src/utils/commands/point.ts
@@ -0,0 +1,21 @@
+import { createEmbedFiled } from "../embed.ts"
+
+// Number of content displayed on the page.
+export const CONTENTS_LIMIT = 10
+
+// deno-lint-ignore no-explicit-any
+export const generateMessageFields = (
+  contents: any[],
+  limit: number,
+  offset: number,
+) => (
+  contents.slice(offset, offset + limit).map((v) =>
+    createEmbedFiled({
+      name: `${v.bonus}%`,
+      value: `${String(v.score.toLocaleString())} - ${
+        (v.score + 19999).toLocaleString()
+      }`,
+      inline: false,
+    })
+  )
+)
